bdbbb44af9249f920ec1f099afe3980146b9af24
Billie Ko <billie.ko@trustarc.com>
2020-03-17 16:50:30 +0800

    Rust: Add bob and gigasecond exercises

---
+++ rust/bob/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn reply(message: &str) -> &str {
+  let trimmed = message.trim();
+
+  // If it ends with question mark
+  let a_question = trimmed.ends_with("?");
+
+  // If it contains any letter in the alphabet
+  let has_letter = trimmed.contains(char::is_alphabetic);
+
+  // If it contains any letter and all letters are in uppercase
+  let a_yell = has_letter && trimmed == trimmed.to_ascii_uppercase();
+
+  if trimmed.is_empty() {
+    "Fine. Be that way!"
+  }
+  else if a_yell && a_question {
+    "Calm down, I know what I'm doing!"
+  }
+  else if a_yell {
+    "Whoa, chill out!"
+  }
+  else if a_question {
+    "Sure."
+  }
+  else {
+    "Whatever."
+  }
+}
